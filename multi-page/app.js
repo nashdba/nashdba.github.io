12f587043fd3ee2d1832b492f1ec243db6959ee6
nashdba <44735187+nashdba@users.noreply.github.com>
2024-11-15 07:21:47 +0000

    Update app.js

--- multi-page/app.js
+++ multi-page/app.js
@@ -1,5 +1,5 @@
 // Initialize IBM AppID
-const appID = new AppID({
+const appID = new appID({
     clientId: '0a08efad-7177-4907-a2d2-3aa90b4e5af2', // Your IBM App ID client ID
     discoveryEndpoint: 'https://eu-gb.appid.cloud.ibm.com/oauth/v4/2e834006-5b8a-4c84-937b-7a7d6a14ccbf/.well-known/openid-configuration', // Your App ID discovery URL
     redirectUri: window.location.href
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const currentPage = window.location.pathname;
 
     // If user is not authenticated, redirect to login page
-    if (currentPage !== '/login.html' && !appID.isLoggedIn()) {
+    if (currentPage !== '/login.html' && !appID.isAuthenticated()) {
         window.location.href = 'login.html';
     }
 
